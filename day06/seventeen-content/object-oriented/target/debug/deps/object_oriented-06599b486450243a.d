43fea8ddb28f1ec75c56b95c55942e740c1357ba
Apeknockcode <freeman_top@163.com>
2023-11-21 22:55:02 +0800

    打卡 面向对象

---
+++ day06/seventeen-content/object-oriented/target/debug/deps/object_oriented-06599b486450243a.d
@@ -0,0 +1,5 @@
+/Users/taotao/Documents/自学项目/RUST-CODE-SPACE/day06/seventeen-content/object-oriented/target/debug/deps/object_oriented-06599b486450243a.rmeta: src/main.rs
+
+/Users/taotao/Documents/自学项目/RUST-CODE-SPACE/day06/seventeen-content/object-oriented/target/debug/deps/object_oriented-06599b486450243a.d: src/main.rs
+
+src/main.rs:
---
+++ day06/seventeen-content/object-oriented/target/debug/deps/object_oriented-ce4d288040323cfc.d
@@ -0,0 +1,5 @@
+/Users/taotao/Documents/自学项目/RUST-CODE-SPACE/day06/seventeen-content/object-oriented/target/debug/deps/object_oriented-ce4d288040323cfc.rmeta: src/main.rs
+
+/Users/taotao/Documents/自学项目/RUST-CODE-SPACE/day06/seventeen-content/object-oriented/target/debug/deps/object_oriented-ce4d288040323cfc.d: src/main.rs
+
+src/main.rs:
