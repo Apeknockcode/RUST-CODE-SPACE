da7fd5fb90fe4484d6c8e6bc7185527c29fa6047
陶建 <freeman_top@163.com>
2023-12-04 20:46:37 +0800

    打卡 综合练习题

--- Exercise Library/rustlings/exercises/quiz2.rs
+++ Exercise Library/rustlings/exercises/quiz2.rs
@@ -20,23 +20,34 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
 
 pub enum Command {
-    Uppercase,
-    Trim,
-    Append(usize),
+    Uppercase,  // 转化为大写
+    Trim, // 去除空格
+    Append(usize), // 添加
 }
 
 mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = Vec::with_capacity(input.len());
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command{
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                },
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                },
+                Command::Append(n) => {
+                    output.push(format!("{}{}", string, "bar".repeat(*n)));
+                }
+            }
         }
         output
     }
@@ -45,7 +56,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
